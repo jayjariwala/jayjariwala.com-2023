4f9f4b3fd8797b29e88c11be12ecf6419b228b60
Jay Jariwala <jay.jariwala@hotmail.com>
2023-11-13 15:44:49 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `jayjariwala.com-2023`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
