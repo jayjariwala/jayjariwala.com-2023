280369a80fee20adcffae9c77f07f7e0f583cebc
jay.jariwala <jay.jariwala@cvshealth.com>
2023-11-13 16:42:23 -0500

    resolve relative path vs absolute path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+const path = require('path');
 module.exports = {
-  siteMetadata: {
-    title: `jayjariwala.com-2023`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-postcss"]
+	siteMetadata: {
+		title: `jayjariwala.com-2023`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-resolve-src',
+			options: {
+				srcPath: path.resolve(__dirname, 'src'),
+			},
+		},
+		'gatsby-plugin-postcss',
+	],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,11 @@ import {
 	Technologies,
 	Card,
 	Round,
-} from '../components/styles/Hero.module.css';
-import { Medium } from '../components/styles/Container.module.css';
-import { Btn, Btn_primary, Btn_lg } from '../components/styles/Button.module.css';
-import { M6, M2 } from '../components/styles/Margin.module.css';
-import avatar from '../assets/images/avatar1.jpg';
+} from 'components/styles/Hero.module.css';
+import { Medium } from 'components/styles/Container.module.css';
+import { Btn, Btn_primary, Btn_lg } from 'components/styles/Button.module.css';
+import { M6, M2 } from 'components/styles/Margin.module.css';
+import avatar from 'assets/images/avatar1.jpg';
 
 const IndexPage = () => {
 	return (
@@ -27,7 +27,7 @@ const IndexPage = () => {
 			<div className={Hero}>
 				<div className={Overlay}></div>
 				<div className={`${Medium} ${introSection} text-center`}>
-					<img className={Avatar} src={avatar} width="200px" height="200px" />
+					<img className={Avatar} src={avatar} width="200px" height="200px" alt="avatar logo" />
 					<div className={Introduction}>
 						<p className={Hi}>Hello there!</p>
 						<h1 className={WhoAmI}>
@@ -36,12 +36,10 @@ const IndexPage = () => {
 						</h1>
 						<div className={SubIntro}>
 							<p>
-								As a tech enthusiast and software engineer, my passion lies in leveraging cutting-edge technology to
-								create solutions that positively impact on people's lives. With expertise in healthcare technology, I
-								currently serve as a full stack software engineer at CVS health, having previously contributed to
-								Tausight as a User Interface engineer. I thrive on crafting innovative software to address real-world
-								challenges, aiming to contribute to a tech-driven world that enhances and simplifies the human
-								experience.
+								As a tech enthusiast and software engineer, my passion lies in leveraging cutting-edge technology to create solutions that positively
+								impact on people's lives. With expertise in healthcare technology, I currently serve as a full stack software engineer at CVS health,
+								having previously contributed to Tausight as a User Interface engineer. I thrive on crafting innovative software to address real-world
+								challenges, aiming to contribute to a tech-driven world that enhances and simplifies the human experience.
 							</p>
 						</div>
 						<div className={M6}>
@@ -62,8 +60,8 @@ const IndexPage = () => {
 					</div>
 					<h3>UI Design</h3>
 					<p>
-						I love beautiful interfaces with great typography and vibrant colors. I design websites and web application
-						mockups using Sketch/Figma and convert them into high-fidelity prototype
+						I love beautiful interfaces with great typography and vibrant colors. I design websites and web application mockups using Sketch/Figma and
+						convert them into high-fidelity prototype
 					</p>
 				</div>
 				<div className={Card}>
@@ -72,8 +70,8 @@ const IndexPage = () => {
 					</div>
 					<h3>Frontend Development</h3>
 					<p>
-						I develop pixel perfect, cross browser compatible, and responsive websites using HTML5, CSS3 and JavaScript.
-						I also develop Single Page Application frontend using React and Angular
+						I develop pixel perfect, cross browser compatible, and responsive websites using HTML5, CSS3 and JavaScript. I also develop Single Page
+						Application frontend using React and Angular
 					</p>
 				</div>
 				<div className={Card}>
@@ -82,8 +80,8 @@ const IndexPage = () => {
 					</div>
 					<h3>Backend Development</h3>
 					<p>
-						My expertise spans designing and maintaining databases, implementing robust application logic, and ensuring
-						seamless integration between front-end and back-end components.
+						My expertise spans designing and maintaining databases, implementing robust application logic, and ensuring seamless integration between
+						front-end and back-end components.
 					</p>
 				</div>
 			</div>
