103cb1fb82de275dc089c0efcf7c8f3407fc1b4d
jay.jariwala <jay.jariwala@cvshealth.com>
2023-12-10 15:53:47 -0500

    fix css naming conventions

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,45 +3,45 @@ import { Layout } from '../components/layouts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGem, faPaintBrush, faDatabase } from '@fortawesome/free-solid-svg-icons';
 import {
-	Hero,
-	Overlay,
-	Avatar,
+	hero,
+	overlay,
+	avatar,
 	introSection,
-	Introduction,
-	Hi,
-	WhoAmI,
-	SubIntro,
-	Section1,
-	Section2,
-	Section3,
-	Technologies,
-	Card,
-	Round,
-	ContactCard,
-	ContactFormHeading,
-	ContactForm,
-	InputType,
+	introduction,
+	hi,
+	whoAmI,
+	subIntro,
+	section1,
+	section2,
+	section3,
+	technologies,
+	card,
+	round,
+	contactCard,
+	contactFormHeading,
+	contactForm,
+	inputType,
 } from 'components/styles/Home.module.css';
-import { Medium } from 'components/styles/Container.module.css';
-import { Btn, Btn_primary, Btn_lg, Btn_sm, Link_btn, ButtonGroup } from 'components/styles/Button.module.css';
+import { medium } from 'components/styles/Container.module.css';
+import { btn, btnPrimary, btnLg, btnSm, linkBtn, buttonGroup } from 'components/styles/Button.module.css';
 import { M6, M2 } from 'components/styles/Margin.module.css';
-import avatar from 'assets/images/avatar2.jpg';
+import avatarImg from 'assets/images/avatar2.jpg';
 import { Link } from 'gatsby';
-
+import Button from '@mui/material/Button';
 const IndexPage = () => {
 	return (
 		<Layout>
-			<div className={Hero}>
-				<div className={Overlay}></div>
-				<div className={`${Medium} ${introSection} text-center`}>
-					<img className={Avatar} src={avatar} alt="avatar logo" />
-					<div className={Introduction}>
-						<p className={Hi}>Hello there!</p>
-						<h1 className={WhoAmI}>
+			<div className={hero}>
+				<div className={overlay}></div>
+				<div className={`${medium} ${introSection} text-center`}>
+					<img className={avatar} src={avatarImg} alt="avatar logo" />
+					<div className={introduction}>
+						<p className={hi}>Hello there!</p>
+						<h1 className={whoAmI}>
 							I am Jay Jariwala. Full Stack JavaScript Developer with passion for {''}
 							<span className="highlight">Design</span>
 						</h1>
-						<div className={SubIntro}>
+						<div className={subIntro}>
 							<p>
 								As a tech enthusiast and software engineer, my passion lies in leveraging cutting-edge technology to create solutions that positively
 								impact on people's lives. With expertise in healthcare technology, I currently serve as a full stack software engineer at CVS health,
@@ -49,22 +49,22 @@ const IndexPage = () => {
 								challenges, aiming to contribute to a tech-driven world that enhances and simplifies the human experience.
 							</p>
 						</div>
-						<div className={`${M6} ${ButtonGroup} `}>
-							<Link to="#contact-me" className={`${Btn} ${Btn_primary} ${Btn_lg} ${Link_btn} `}>
+						<div className={`${M6} ${buttonGroup} `}>
+							<Link to="#contact-me" className={`${btn} ${btnPrimary} ${btnLg} ${linkBtn} `}>
 								Get in Touch
 							</Link>
-							<button className={`${Btn} ${Btn_lg}`}>Resume</button>
+							<button className={`${btn} ${btnLg}`}>Resume</button>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className={`${Section1} `}>
+			<div className={`${section1} `}>
 				<h2>I love working with cutting edge technolgies</h2>
 				<p className={M2}>Things I am good at...</p>
 			</div>
-			<div className={`${Technologies} ${Medium}`}>
-				<div className={Card}>
-					<div className={Round}>
+			<div className={`${technologies} ${medium}`}>
+				<div className={card}>
+					<div className={round}>
 						<FontAwesomeIcon icon={faGem} size="2x" />
 					</div>
 					<h3>UI Design</h3>
@@ -73,8 +73,8 @@ const IndexPage = () => {
 						convert them into high-fidelity prototype
 					</p>
 				</div>
-				<div className={Card}>
-					<div className={Round}>
+				<div className={card}>
+					<div className={round}>
 						<FontAwesomeIcon icon={faPaintBrush} size="2x" />
 					</div>
 					<h3>Frontend Development</h3>
@@ -83,8 +83,8 @@ const IndexPage = () => {
 						Application frontend using React and Angular
 					</p>
 				</div>
-				<div className={Card}>
-					<div className={Round}>
+				<div className={card}>
+					<div className={round}>
 						<FontAwesomeIcon icon={faDatabase} size="2x" />
 					</div>
 					<h3>Backend Development</h3>
@@ -94,26 +94,28 @@ const IndexPage = () => {
 					</p>
 				</div>
 			</div>
-			<div className={Section2}>
+			<div className={section2}>
 				<h2>Work Experience</h2>
 				<div className="">
-					<div></div>
+					<div>
+						<Button variant="contained">Hello world</Button>
+					</div>
 					<div></div>
 				</div>
 			</div>
-			<div className={`${Section3}`}>
-				<div className={ContactCard}>
-					<h3 className={ContactFormHeading} id="contact-me">
+			<div className={`${section3}`}>
+				<div className={contactCard}>
+					<h3 className={contactFormHeading} id="contact-me">
 						Contact me
 					</h3>
-					<form method="post" action="https://getform.io/f/62a7def0-d14f-4596-bc6d-ab3d746e4080" className={ContactForm}>
+					<form method="post" action="https://getform.io/f/62a7def0-d14f-4596-bc6d-ab3d746e4080" className={contactForm}>
 						<label htmlFor="name">Name</label>
-						<input type="text" id="name" name="name" className={InputType} />
+						<input type="text" id="name" name="name" className={inputType} />
 						<label htmlFor="email">Email</label>
-						<input type="email" id="email" name="email" className={InputType} />
+						<input type="email" id="email" name="email" className={inputType} />
 						<label htmlFor="message">Message</label>
-						<textarea rows={10} className={InputType} name="message" id="message" placeholder="Write something.." />
-						<button type="submit" className={`${Btn} ${Btn_primary} ${Btn_sm} `}>
+						<textarea rows={10} className={inputType} name="message" id="message" placeholder="Write something.." />
+						<button type="submit" className={`${btn} ${btnPrimary} ${btnSm} `}>
 							Send
 						</button>
 					</form>
